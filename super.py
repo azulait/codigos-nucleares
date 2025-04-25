093b5827b50017a0e141d81ce8a866406132996c
azulait <m.medina4@duocuc.cl>
2025-04-25 18:45:10 -0400

    25/04

--- super.py
+++ super.py
@@ -1,2 +1,13 @@
 palabra=input("diga la palabra ")
-print(len(palabra))
+caract=0
+vocal=0
+cons=0
+for i in str (palabra):
+    caract+=1
+    if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
+        vocal+=1
+    elif i!= " ":
+        cons+=1
+
+print(caract)
+print(f"vocales= {vocal} \n consonantes={cons}")
